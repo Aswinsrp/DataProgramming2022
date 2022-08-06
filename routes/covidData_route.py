11b3ecede075b27ea1ea8098a1aa4b42defbf968
Ashwin Rajapandian <ashwin20.95@gmail.com>
2022-08-05 22:28:11 -0400

    fav icon changes

--- routes/covidData_route.py
+++ routes/covidData_route.py
@@ -1,16 +1,24 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, FastAPI
 from config.db import collection_name
 from models.covidData_model import Cases
 from schema.covidData_schema import covidData_serializer, covidDatas_serializer
-from pymongo import MongoClient
+from fastapi.responses import FileResponse
+import os
 
 router = APIRouter()
+app = FastAPI()
 
 @router.get('/healthCheck')
-def doHealthCheck():
+async def doHealthCheck():
    return {"status": "Application running"}
    
 @router.get('/getCovidData')
-def getData():
+async def getData():
      data = covidDatas_serializer(collection_name.find())
      return {"data": data}
+
+@app.get('/favicon.ico')
+async def favicon():
+    file_name = "favicon.png"
+    file_path = os.path.join(app.root_path, "images")
+    return FileResponse(path=file_path, headers={"Content-Disposition": "attachment; filename=" + file_name})
